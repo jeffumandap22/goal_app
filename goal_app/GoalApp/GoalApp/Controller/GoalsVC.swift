89f401b53a8bb20884028d4bca740e3f08a90b3c
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-09 15:10:28 +0800

    Goal Core Data Entity and Attributes

--- goal_app/GoalApp/GoalApp/Controller/GoalsVC.swift
+++ goal_app/GoalApp/GoalApp/Controller/GoalsVC.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
     @IBAction func goalBtnPress(_ sender: Any) {
         print("Button Pressed")

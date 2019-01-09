613a5cb727e897fb5b8c93f6bdd0f364314e89c7
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-09 15:00:21 +0800

    GoalCell created

---
+++ goal_app/GoalApp/GoalApp/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  GoalApp
+//
+//  Created by Jeffrey Umandap on 09/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescLabel: UILabel!
+    
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+ 
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+    }
+}

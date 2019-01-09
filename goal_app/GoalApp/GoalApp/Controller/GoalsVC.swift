c62ba57656470688652b5138fb7cc444d9428e20
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-09 15:27:00 +0800

    Displaying static goal cells in UITableView / Creating Goal Type Enum

--- goal_app/GoalApp/GoalApp/Controller/GoalsVC.swift
+++ goal_app/GoalApp/GoalApp/Controller/GoalsVC.swift
@@ -10,14 +10,34 @@ import UIKit
 import CoreData
 
 class GoalsVC: UIViewController {
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
     @IBAction func goalBtnPress(_ sender: Any) {
         print("Button Pressed")
+    }
+}
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week", type: .shortTerm, goalProgressAmount: 2)
+        return cell
     }
 }
 
---
+++ goal_app/GoalApp/GoalApp/Enums/GoalType.swift
@@ -0,0 +1,15 @@
+//
+//  GoalType.swift
+//  GoalApp
+//
+//  Created by Jeffrey Umandap on 09/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+    
+}

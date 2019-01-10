6af6f94917f9ebd9fa8741e32abbf00e164c9b16
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-10 12:04:56 +0800

    fixed Dismissal of FinishGoalVC

--- goal_app/GoalApp/GoalApp/Controller/CreateGoalVC.swift
+++ goal_app/GoalApp/GoalApp/Controller/CreateGoalVC.swift
@@ -46,7 +46,7 @@ class CreateGoalVC: UIViewController, UITextViewDelegate {
             guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else { return }
             finishGoalVC.initData(description: goalTextView.text, type: goalType)
             
-            presentDetail(finishGoalVC)
+            presentingViewController?.presentSecondaryDetail(finishGoalVC)
         }
     }
     
